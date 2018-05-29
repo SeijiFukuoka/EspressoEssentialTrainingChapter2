815e4ae3e521cd79970a9f38505014743bef1d8a
Seiji Fukuoka <seijifukuoka@gmail.com>
2018-05-29 16:29:28 -0300

    Add button behaviour

--- app/src/androidTest/java/com/sqisland/android/espresso/hello/MainActivityTest.kt
+++ app/src/androidTest/java/com/sqisland/android/espresso/hello/MainActivityTest.kt
@@ -3,10 +3,10 @@ package com.sqisland.android.espresso.hello
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.assertion.ViewAssertions.matches
-import android.support.test.espresso.matcher.ViewMatchers.withId
-import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import org.hamcrest.CoreMatchers.not
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -25,6 +25,7 @@ class MainActivityTest {
 
         onView(withId(R.id.greet_button))
                 .perform(click())
+                .check(matches(not(isEnabled())))
 
         onView(withId(R.id.greeting))
                 .check(matches(withText(R.string.hello)))
--- app/src/main/java/com/sqisland/android/espresso/hello/MainActivity.kt
+++ app/src/main/java/com/sqisland/android/espresso/hello/MainActivity.kt
@@ -7,14 +7,17 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var greetingView: TextView
+    private lateinit var greetingButton: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         greetingView = findViewById(R.id.greeting)
+        greetingButton = findViewById(R.id.greet_button)
     }
 
     fun greet(v: View) {
         greetingView.setText(R.string.hello)
+        greetingButton.isEnabled = false
     }
 }
